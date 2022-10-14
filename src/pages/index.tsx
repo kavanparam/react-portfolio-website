d9d234bb9b80d6173193e463d91a5b79f25e8c42
kavanparam <kavan.param@gmail.com>
2022-10-14 03:52:54 -0400

    Rm unused code (**to be revisited later)
    - wanted to refactor this for my use cases but will do this after initial site setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,67 +4,6 @@ import { Link, animateScroll as scroll } from "react-scroll";
 import type { HeadFC } from "gatsby";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
-// Refactor to social links
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  },
-];
-
-// Refactor for projects — can store description, title, link in this format, and maybe features as well to be formatted into grid card style format
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-];
-
 type WindowProps = {
   x: undefined | Number;
   y: undefined | Number;
@@ -159,39 +98,7 @@ const IndexPage = () => {
             </li>
           </ul>
         </section>
-        {/* <ul style={doclistStyles}>
-        {docLinks.map((doc) => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul> */}
 
-        {/* <ul style={listStyles}>
-        {links.map((link) => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul> */}
         <section className="z-20 p-8 -mb-10 drop-shadow-[0px_0px_50px_rgba(240,150,10,0.2)] rounded-3xl bg-zinc-100">
           <div className="h-screen">
             <h2 id="projects" className="mb-8 text-2xl font-thin">
@@ -246,7 +153,8 @@ const IndexPage = () => {
           </code>
           <ul className="mb-24 ml-4 font-light list-disc">
             <li>
-              <s>watch the react tailwind tutorial by dev ed</s>
+              <s>watch the react tailwind tutorial by dev ed</s> — setup dark
+              mode toggle
             </li>
             <li>setup parallax scrolling</li>
             <li>import projects</li>
