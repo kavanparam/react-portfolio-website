1d39dcf4b7964a4a2d94187d696d2e70d5d714ae
kavanparam <kavan.param@gmail.com>
2022-10-14 16:29:19 -0400

    Rm classNames from sections
    - using them on the ParallaxLayers instead
    - seems to be working, might need to rm the 'h-screen' styling as well
    - imported a cover photo to be used on welcome page (for time being)
    - need to add speeds to the ParallaxLayers to take advantage of the stacking effect when scrolling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,112 +54,134 @@ const IndexPage = () => {
         </ul>
       </nav>
 
-      <main className="h-screen font-main">
-        <section className="z-10 h-screen p-12 -mb-10 bg-zinc-50 rounded-2xl">
-          <h1 className="mt-40 mb-8 text-5xl font-bold drop-shadow-md">
-            Hi 👋🏼, I'm <span className="text-amber-500">Kavan</span>
-          </h1>
-          <ul className="flex gap-2 font-mono">
-            <li>
-              <Link
-                className="underline cursor-pointer underline-offset-4 decoration-0"
-                activeClass="active"
-                to="projects"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={800}
-              >
-                works
-              </Link>
-              ,
-            </li>
-            <li>
-              <Link
-                className="underline cursor-pointer underline-offset-4 decoration-0"
-                activeClass="active"
-                to="photography"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={800}
-              >
-                photography
-              </Link>
-              ,
-            </li>
-            <li>
-              <a
-                className="underline underline-offset-4 decoration-0"
-                href="/about"
-              >
-                about
-              </a>
-            </li>
-          </ul>
-        </section>
+      <main className="font-main">
+        <Parallax pages={5}>
+          {/* may need to rm section & h-screens for this to work as expected */}
 
-        <section className="z-20 p-8 -mb-10 drop-shadow-[0px_0px_50px_rgba(240,150,10,0.2)] rounded-3xl bg-zinc-100">
-          <div className="h-screen">
-            <h2 id="projects" className="mb-8 text-2xl font-thin">
-              my projects
-            </h2>
-            <div className="mb-96">
-              <h3 className="text-3xl font-bold text-center drop-shadow-2xl">
-                <span className="text-transparent bg-gradient-to-r from-sky-500 to-blue-600 bg-clip-text">
-                  Multiplication
-                </span>{" "}
-                <span className="text-transparent bg-gradient-to-r from-amber-500 to-orange-400 bg-clip-text">
-                  Connect
-                </span>{" "}
-                <span className="text-transparent bg-gradient-to-r bg-clip-text from-red-500 to-red-700">
-                  Four
-                </span>
-              </h3>
-            </div>
-            <div className="mb-96">
-              <h3 className="text-3xl font-bold text-center drop-shadow-2xl text-fuchsia-500 ">
-                Portfolio Website
-              </h3>
-            </div>
-          </div>
+          <ParallaxLayer className="z-10 h-screen p-12 bg-zinc-50 rounded-2xl">
+            <section>
+              <h1 className="mt-40 mb-8 text-5xl font-bold drop-shadow-md">
+                Hi 👋🏼, I'm <span className="text-amber-500">Kavan</span>
+              </h1>
+              <ul className="flex gap-2 font-mono">
+                <li>
+                  <Link
+                    className="underline cursor-pointer underline-offset-4 decoration-0"
+                    activeClass="active"
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    offset={-50}
+                    duration={800}
+                  >
+                    works
+                  </Link>
+                  ,
+                </li>
+                <li>
+                  <Link
+                    className="underline cursor-pointer underline-offset-4 decoration-0"
+                    activeClass="active"
+                    to="photography"
+                    spy={true}
+                    smooth={true}
+                    offset={-50}
+                    duration={800}
+                  >
+                    photography
+                  </Link>
+                  ,
+                </li>
+                <li>
+                  <a
+                    className="underline underline-offset-4 decoration-0"
+                    href="/about"
+                  >
+                    about
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </ParallaxLayer>
 
-          <div className="h-screen">
-            <div className="mb-96">
-              <h3 className="text-3xl font-bold text-center text-transparent drop-shadow-2xl bg-gradient-to-t from-amber-500 to-orange-600 bg-clip-text">
-                Pokedex App
-              </h3>
-            </div>
-            <div className="mb-96">
-              <h3 className="text-3xl font-bold text-center text-transparent drop-shadow-2xl bg-gradient-to-r from-cyan-600 to-red-600 bg-clip-text">
-                BookStore App
-              </h3>
-            </div>
-          </div>
-        </section>
+          <ParallaxLayer
+            offset={1}
+            factor={2}
+            className="z-20 h-screen p-8 drop-shadow-[0px_0px_50px_rgba(240,150,10,0.2)] rounded-3xl bg-gradient-to-b from-zinc-100 to-zinc-200"
+          >
+            <section>
+              <div className="">
+                <h2 id="projects" className="mb-8 text-2xl font-thin">
+                  my projects
+                </h2>
+                <div className="mb-96">
+                  <h3 className="text-3xl font-bold text-center drop-shadow-2xl">
+                    <span className="text-transparent bg-gradient-to-r from-sky-500 to-blue-600 bg-clip-text">
+                      Multiplication
+                    </span>{" "}
+                    <span className="text-transparent bg-gradient-to-r from-amber-500 to-orange-400 bg-clip-text">
+                      Connect
+                    </span>{" "}
+                    <span className="text-transparent bg-gradient-to-r bg-clip-text from-red-500 to-red-700">
+                      Four
+                    </span>
+                  </h3>
+                </div>
+                <div className="mb-96">
+                  <h3 className="text-3xl font-bold text-center drop-shadow-2xl text-fuchsia-500 ">
+                    Portfolio Website
+                  </h3>
+                </div>
+              </div>
 
-        <section className="z-20 h-screen p-8 -mb-10 drop-shadow-[0px_0px_50px_rgba(240,150,10,0.2)] rounded-3xl bg-zinc-200">
-          <h2 id="projects" className="mb-8 text-2xl font-thin">
-            recent github activity
-          </h2>
-        </section>
+              <div className="">
+                <div className="mb-96">
+                  <h3 className="text-3xl font-bold text-center text-transparent drop-shadow-2xl bg-gradient-to-t from-amber-500 to-orange-600 bg-clip-text">
+                    Pokedex App
+                  </h3>
+                </div>
+                <div className="">
+                  <h3 className="text-3xl font-bold text-center text-transparent drop-shadow-2xl bg-gradient-to-r from-cyan-600 to-red-600 bg-clip-text">
+                    BookStore App
+                  </h3>
+                </div>
+              </div>
+            </section>
+          </ParallaxLayer>
 
-        <section className="z-30 h-screen p-12 -mb-5 bg-zinc-300 rounded-3xl drop-shadow-[0px_0px_50px_rgba(240,150,10,0.2)]">
-          <h2 className="max-w-xs mb-16 text-3xl font-bold">
-            Page <span className="text-gatsby-purple">Build ⚡️</span>
-          </h2>
-          <code className="p-1 text-xl underline rounded underline-offset-4 decoration-0 bg-gatsby-bg-code text-gatsby-purple">
-            outline
-          </code>
-          <ul className="mb-24 ml-4 font-light list-disc">
-            <li>
-              <s>watch the react tailwind tutorial by dev ed</s> — setup dark
-              mode toggle
-            </li>
-            <li>setup parallax scrolling</li>
-            <li>import projects</li>
-          </ul>
-        </section>
+          <ParallaxLayer
+            offset={3}
+            className="z-20 h-screen p-8 drop-shadow-[0px_0px_50px_rgba(240,150,10,0.2)] rounded-3xl bg-zinc-300"
+          >
+            <section>
+              <h2 id="projects" className="mb-8 text-2xl font-thin">
+                recent github activity
+              </h2>
+            </section>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={4}
+            className="z-30 h-screen p-12 bg-zinc-400 rounded-3xl drop-shadow-[0px_0px_50px_rgba(240,150,10,0.2)]"
+          >
+            <section>
+              <h2 className="max-w-xs mb-16 text-3xl font-bold">
+                Page <span className="text-gatsby-purple">Build ⚡️</span>
+              </h2>
+              <code className="p-1 text-xl underline rounded underline-offset-4 decoration-0 bg-gatsby-bg-code text-gatsby-purple">
+                outline
+              </code>
+              <ul className="mb-24 ml-4 font-light list-disc">
+                <li>
+                  <s>watch the react tailwind tutorial by dev ed</s> — setup
+                  dark mode toggle
+                </li>
+                <li>setup parallax scrolling</li>
+                <li>import projects</li>
+              </ul>
+            </section>
+          </ParallaxLayer>
+        </Parallax>
       </main>
     </div>
   );
