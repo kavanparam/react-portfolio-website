3a67bf4f3fbc7fe40ccccae7774d732ba78d9eee
kavanparam <kavan.param@gmail.com>
2022-10-19 03:56:21 -0400

    trying to find a solution to use on main page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,11 +6,16 @@ const about = () => {
       <h1>about</h1>
 
       <h3>grid testing</h3>
-      <section className="relative grid grid-cols-none grid-rows-3 gap-10 border">
-        <div className="absolute w-full row-start-1 row-end-2">1</div>
-        <div className="row-start-2 row-end-3">2</div>
-        <div className="row-start-3 row-end-4">3</div>
-      </section>
+      <main className="relative grid grid-rows-3 gap-10 border">
+        {/* can't set a height on this or it overflows */}
+        <div className="absolute w-full h-full row-start-1 row-end-2 bg-red-400">
+          <section className="h-screen">1</section>
+        </div>
+        <div className="absolute w-full row-start-2 row-end-3">2</div>
+        <div className="w-full row-start-3 row-end-4 ">3</div>
+      </main>
+
+      {/* might want to use absolute elements and calculated margins instead like KP tutorial */}
     </div>
   );
 };
