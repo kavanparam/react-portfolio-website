f9c6f392d374d2c258f84fd51ea12d155da53fe5
kavanparam <kavan.param@gmail.com>
2022-10-20 02:35:02 -0400

    build outlining
    - rm completed work in page build
    - ready for merge to main — this build is mobile friendly
    - adding content next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -121,7 +121,7 @@ const IndexPage = () => {
             duration: 1,
           }}
           viewport={{ amount: 0.15, once: false }}
-          className=" z-20 p-[4%] min-h-fit w-full mb-96 shadow-divUp rounded-3xl bg-gradient-to-b from-zinc-100 to-zinc-200"
+          className=" z-20 p-[4%] min-h-screen w-full mb-96 shadow-divUp rounded-3xl bg-gradient-to-b from-zinc-100 to-zinc-200"
         >
           <h2 id="projects" className="mb-8 text-2xl font-thin">
             my projects
@@ -169,7 +169,7 @@ const IndexPage = () => {
             duration: 1,
           }}
           viewport={{ amount: 0.3, once: false }}
-          className="w-full z-30 p-[4%] h-screen mb-96 shadow-divUp rounded-3xl bg-zinc-300"
+          className="w-full z-30 p-[4%] min-h-screen mb-96 shadow-divUp rounded-3xl bg-zinc-300"
         >
           <h2 id="projects" className="mb-8 text-2xl font-thin">
             recent github activity
@@ -185,7 +185,7 @@ const IndexPage = () => {
             duration: 1,
           }}
           viewport={{ amount: 0.3, once: false }}
-          className="z-40 p-[8%] bg-zinc-400 rounded-3xl shadow-divUp overflow-scroll"
+          className="z-40 min-h-screen p-[8%] bg-zinc-400 rounded-3xl shadow-divUp overflow-scroll"
         >
           <h2 className="mb-8 text-3xl font-bold">
             Upcoming Page
@@ -194,19 +194,14 @@ const IndexPage = () => {
           <code className="p-1 text-lg text-orange-900 underline bg-yellow-100 rounded underline-offset-4 decoration-0 ">
             outline
           </code>
-          <ul className="mt-2 mb-24 ml-4 font-light list-disc marker:text-purple-900">
+          <ul className="mt-2 ml-4 font-light list-disc marker:text-purple-900">
             <li>
               <s>watch the react tailwind tutorial by dev ed</s> — setup dark
               mode toggle
             </li>
             <li>import projects</li>
-            <li>
-              setup daisyUI page progress bar when scrolling — fixed, only
-              appears on scroll, horizontal+centered on mobile, vertical off the
-              side on desktop, rm scroll to top after
-            </li>
+            <li>fix mobile navbar w hook-window-resize — supports SSR</li>
             <li>implement more daisy UI — ideas: code, window</li>
-            <li>change navbar font</li>
             <li>
               add a credits section — listing technologies and libraries used
             </li>
@@ -214,55 +209,44 @@ const IndexPage = () => {
               clean up code — extract this list into an object like initial
               gatsby formatting
             </li>
-            <li className="mt-4">
-              rm unused npm packages w npm-check library or uninstall command —
-              react-spring, react-scroll
-            </li>
+            <li>change navbar font</li>
+            <li>consider adding snapping functionality</li>
+
             <li>
               <h5 className="font-bold ">Move to Framer Motion</h5>
-              <p>
-                Oversight using the current library for Parallax Effects.
-                <br />
-                “Pages” are defined in ParallaxLayer in react-spring with a
-                factor prop. This isn’t proportional to the content inside the
-                ParallaxLayer. Meaning that content overflows on mobile with
-                current implementation in this project, and isn’t easily
-                fixable.
-              </p>
-              <ul className="mt-2 ml-4 list-disc list-inside">
+              <ul className="mt-1 ml-4 list-disc list-inside">
                 <li>
-                  setup parallax w previous overlapping behaviour (& spring
-                  effect)
+                  rm unused npm packages w npm-check library or uninstall
+                  command — react-spring, react-scroll
                 </li>
                 <li>
                   make links functional again (navbar, welcome, footer scroll to
                   top)
                 </li>
+                <li>
+                  (labeled w clickable links) progress bar w Framer
+                  — amber-400/500
+                  <details className="ml-8">
+                    <summary>previous idea (use the headings)</summary>
+                    <ul className="steps steps-horizontal">
+                      <li className="step step-primary">welcome</li>
+                      <li className="step step-primary">project list</li>
+                      <li className="step">github activity</li>
+                      <li className="step">future page builds</li>
+                    </ul>
+                    <li>
+                      auto-hide on mobile when not in use (clickable links)
+                    </li>
+                    <li>
+                      use md:steps-vertical and pin off the side for larger
+                      screens
+                    </li>
+                    <li>rm scroll to top once this is functional</li>
+                  </details>
+                </li>
               </ul>
             </li>
           </ul>
-
-          <h4 className="text-xl font-semibold">
-            Page Progress Bar — to be built
-          </h4>
-          <h5 className="underline">Build</h5>
-          <ul className="ml-6 list-disc list-outside marker:text-amber-700 marker:content-['+_'] marker:font-bold   ">
-            <li>make sections clickable</li>
-            <li>rm scroll to top once this is functional</li>
-            <li>auto-hide on mobile</li>
-            <li>
-              use md:steps-vertical and pin off the side for larger screens
-            </li>
-            <li>might want to replace with a labeled progress bar w Framer</li>
-          </ul>
-          <div className="mt-4 text-center">
-            <ul className="steps steps-horizontal">
-              <li className="step step-primary">welcome</li>
-              <li className="step step-primary">project list</li>
-              <li className="step">github activity</li>
-              <li className="step">future page builds</li>
-            </ul>
-          </div>
         </motion.section>
       </main>
 
