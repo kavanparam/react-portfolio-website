064ccd3c6cd974e5570e080d236152c7bdbe0db5
kavanparam <kavan.param@gmail.com>
2022-10-20 02:43:35 -0400

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,6 +3,7 @@ import React from "react";
 type Props = {};
 
 const about = (props: Props) => {
+  // animate down onto page OR push page to the right on entry w FM
   return <div>about</div>;
 };
 
