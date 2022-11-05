283583a0d948eb84d6244a34be9be167f4a6b350
kavanparam <kavan.param@gmail.com>
2022-11-05 17:10:39 -0400

    navbar rework & progress bar styles
    - move to traditional navbar to avoid issues
    - issues bw chrome based browsers and Safari, switch back to normal navbar
    - removed rendering in progress bar (distracting)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
 import * as React from "react";
 import { useEffect, useRef, useState } from "react";
 import type { HeadFC } from "gatsby";
-import {
-  motion,
-  useScroll,
-  useTransform,
-  useInView,
-  useSpring,
-  AnimatePresence,
-} from "framer-motion";
+import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import { StaticImage } from "gatsby-plugin-image";
 import { Link, animateScroll as scroll } from "react-scroll";
 // import { Parallax, ParallaxLayer } from "@react-spring/parallax";
@@ -100,48 +93,44 @@ const IndexPage = () => {
           <div className="w-1 h-1 rounded-md bg-light-black dark:bg-dark-white"></div>
         </button>
         <nav
-          className={`z-50 sm:block sm:w-full fixed font-main ${
+          className={`z-40 sm:block sm:w-full fixed font-main mt-3 ${
             isOpen && size?.x && size.x < 640 ? "right-0 bottom-0" : "hidden"
           }`}
         >
-          <ul className="p-6 mb-32 font-mono text-4xl font-bold text-right uppercase sm:mb-0 sm:p-0 sm:text-start word-spacing-tight sm:text-sm drop-shadow-[0_20px_100px_rgba(0,0,0,0.8)] sm:drop-shadow-none">
-            <li className="sm:fixed sm:top-6 sm:left-6">
+          <ul className="p-6 mb-32 sm:flex sm:items-center sm:gap-8 font-mono text-4xl font-bold text-right uppercase sm:mb-0 sm:p-0 sm:px-8 sm:py-1.5 sm:text-start word-spacing-tight sm:text-sm drop-shadow-[0_20px_100px_rgba(0,0,0,0.8)] sm:drop-shadow-none">
+            <li className="mr-auto">
               <a className="cursor-pointer" onClick={scroll.scrollToTop}>
                 Kavan Paramathasan
               </a>
             </li>
-            <li className="sm:fixed sm:-rotate-90 sm:-bottom-[95vh] sm:left-2">
+            <li className="">
               <a href="/about">about</a>
             </li>
-            <li className="sm:fixed sm:right-0 sm:top-12">
-              <ul className="sm:flex sm:flex-col sm:items-center sm:gap-8">
-                <li className="sm:rotate-90">contact</li>
-                <li>
-                  <label className="swap swap-rotate">
-                    {/* <!-- this hidden checkbox controls the state --> */}
-                    <input
-                      type="checkbox"
-                      onClick={() => setIsDarkMode(!isDarkMode)}
-                    />
-                    {/* <!-- sun icon --> */}
-                    <svg
-                      className="w-10 h-10 fill-current sm:w-6 sm:h-6 swap-on"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                    </svg>
-                    {/* <!-- moon icon --> */}
-                    <svg
-                      className="w-10 h-10 fill-current sm:w-6 sm:h-6 swap-off"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                    </svg>
-                  </label>
-                </li>
-              </ul>
+            <li className="">contact</li>
+            <li>
+              <label className="swap swap-rotate">
+                {/* <!-- this hidden checkbox controls the state --> */}
+                <input
+                  type="checkbox"
+                  onClick={() => setIsDarkMode(!isDarkMode)}
+                />
+                {/* <!-- sun icon --> */}
+                <svg
+                  className="w-10 h-10 fill-current sm:w-6 sm:h-6 swap-on"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                </svg>
+                {/* <!-- moon icon --> */}
+                <svg
+                  className="w-10 h-10 fill-current sm:w-6 sm:h-6 swap-off"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                </svg>
+              </label>
             </li>
           </ul>
         </nav>
@@ -149,28 +138,9 @@ const IndexPage = () => {
 
       <motion.div
         style={{ width }}
-        className="fixed top-0 z-50 flex justify-end h-3 rounded-md shadow-lg bg-gradient-to-l from-amber-500 via-amber-400/50 text-dark-white/90"
+        className="fixed z-50 flex justify-end h-3 shadow-sm rounded-r-md bg-gradient-to-l from-amber-500 to-yellow-300/40 text-dark-white/90"
       >
         <div className="flex items-center mr-2 font-mono text-xs tracking-wider drop-shadow-md">
-          {progressBar > 40 && progressBar < 60 ? (
-            <motion.span
-              initial={{ x: -100 }}
-              animate={{ x: 0 }}
-              className="mt-1 mr-1 text-lg"
-            >
-              🚀
-            </motion.span>
-          ) : (
-            progressBar === 100 && (
-              <motion.span
-                initial={{ opacity: 0, scale: 5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                className="mt-1 mr-1 text-lg"
-              >
-                💯
-              </motion.span>
-            )
-          )}
           <span className="">{progressBar}%</span>
         </div>
       </motion.div>
